e9c25e78907c8a0cd3a7516ade5176bfa9830672
Fikri Kalfov <fikri.kalfov@gmail.com>
2022-10-09 12:19:16 +0300

    Update maxSequenceOfEqualElements.js
    Solution of Task

--- Array Exercise/maxSequenceOfEqualElements.js
+++ Array Exercise/maxSequenceOfEqualElements.js
@@ -1,13 +1,30 @@
 function maxSequenceOfEqualElements(inputArr) {
     let order = '';
-    let counter = 1;
+    let sequance = [];
+        sequance.push(inputArr[0]);
+    let maxSequance = [];
 
     for (let i = 0; i < (inputArr.length - 1); i++) {
+        
 
         if (inputArr[i] === inputArr[i + 1]) {
-            
-
+            sequance.push(inputArr[i + 1]);
+        } else {
+            if (maxSequance.length < sequance.length) {
+                maxSequance = sequance;
+            }
+            sequance = [];
+            sequance.push(inputArr[i + 1]);
         }
 
     }
+
+    for (let i = 0; i < maxSequance.length; i++) {
+        order += maxSequance[i] + ' ';
+    }
+
+    console.log(order);
+
 }
+
+maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
